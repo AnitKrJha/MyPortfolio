13452c06593865fded63700498cb31f746b9ed80
AnitKrJha <anitkjha31@gmail.com>
2022-08-17 20:38:46 +0530

    Added Skills and minor changes

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const sideMenu=document.querySelector('.sidemenu')
-const menu=document.querySelector('.menu')
-const close=document.querySelector('.close')
+const sideMenu = document.querySelector(".sidemenu");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const sideLink = document.querySelector(".sidelink");
 
-window.addEventListener('resize',()=>{
-    sideMenu.style.right='-300px';
-    
-    sideMenu.style.boxShadow='none';
-})
+window.addEventListener("resize", () => {
+  sideMenu.style.right = "-300px";
 
-menu.addEventListener('click',(e)=>{
-    // sideMenu.style.width='300px';
-    sideMenu.style.right='0px';
-    sideMenu.style.boxShadow='0 0 1000px 50px  black';
-})
-close.addEventListener('click',(e)=>{
-    sideMenu.style.right='-300px';
-    // sideMenu.style.width='0px';
-    sideMenu.style.boxShadow='none';
-})
+  sideMenu.style.boxShadow = "none";
+});
+sideLink.addEventListener("click", (e) => {
+  sideMenu.style.right = "-300px";
+  sideMenu.style.boxShadow = "none";
+});
+menu.addEventListener("click", (e) => {
+  // sideMenu.style.width='300px';
+  sideMenu.style.right = "0px";
+  sideMenu.style.boxShadow = "0 0 1000px 50px  black";
+});
+close.addEventListener("click", (e) => {
+  sideMenu.style.right = "-300px";
+  // sideMenu.style.width='0px';
+  sideMenu.style.boxShadow = "none";
+});
